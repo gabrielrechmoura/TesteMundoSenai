17e52d9c0a04759fcaaa2763a19a5cf31d7eb6d8
Gabriel Rech Moura <gabrielrechmoura@gmail.com>
2025-05-23 11:13:25 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,54 +1,54 @@
-var nextBtn = document.querySelector('.next'),
-    prevBtn = document.querySelector('.prev'),
-    carousel = document.querySelector('.carousel'),
-    list = document.querySelector('.list'), 
-    item = document.querySelectorAll('.item'),
-    runningTime = document.querySelector('.carousel .timeRunning') 
+window.onload = function () {
+  var nextBtn = document.querySelector('.next'),
+      prevBtn = document.querySelector('.prev'),
+      carousel = document.querySelector('.carousel'),
+      list = document.querySelector('.list'), 
+      item = document.querySelectorAll('.item'),
+      runningTime = document.querySelector('.carousel .timeRunning');
 
-let timeRunning = 3000 
-let timeAutoNext = 7000
+  let timeRunning = 3000 
+  let timeAutoNext = 7000
 
-nextBtn.onclick = function(){
-    showSlider('next')
-}
+  nextBtn.onclick = function(){
+      showSlider('next')
+  }
 
-prevBtn.onclick = function(){
-    showSlider('prev')
-}
+  prevBtn.onclick = function(){
+      showSlider('prev')
+  }
 
-let runTimeOut 
+  let runTimeOut;
 
-let runNextAuto = setTimeout(() => {
-    nextBtn.click()
-}, timeAutoNext)
+  let runNextAuto = setTimeout(() => {
+      nextBtn.click()
+  }, timeAutoNext)
 
+  function resetTimeAnimation() {
+      if (!runningTime) return; // proteção extra
+      runningTime.style.animation = 'none'
+      runningTime.offsetHeight // trigger reflow
+      runningTime.style.animation = null 
+      runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+  }
 
-function resetTimeAnimation() {
-    runningTime.style.animation = 'none'
-    runningTime.offsetHeight /* trigger reflow */
-    runningTime.style.animation = null 
-    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
-}
+  function showSlider(type) {
+      let sliderItemsDom = list.querySelectorAll('.item');
+      
+      if(type === 'next'){
+          list.appendChild(sliderItemsDom[0]);
+          carousel.classList.add('next');
+      } else {
+          list.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+          carousel.classList.add('prev');
+      }
 
+      clearTimeout(runTimeOut);
 
-function showSlider(type) {
-    let sliderItemsDom = list.querySelectorAll('.item');
-    
-    if(type === 'next'){
-        list.appendChild(sliderItemsDom[0]);
-        carousel.classList.add('next');
-    } else {
-        list.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
-        carousel.classList.add('prev');
-    }
+      runTimeOut = setTimeout(() => {
+          carousel.classList.remove('next');
+          carousel.classList.remove('prev');
+      }, timeRunning);
 
-    clearTimeout(runTimeOut);
-
-    runTimeOut = setTimeout(() => {
-        carousel.classList.remove('next');
-        carousel.classList.remove('prev');
-    }, timeRunning);
-
-   
-    resetTimeAnimation(); // Reset the running time animation
-}
+      resetTimeAnimation();
+  }
+};
